313b1ef509ba5d66fd2402e750ae461e5704a9f0
mirumi <mail@mirumi.tech>
2024-06-24 00:21:03 +0900

    param: `STOP_AFTER_KEYS_FOUND`: 1

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define VANITY_CONFIG
 
 static int const MAX_ITERATIONS = 100000;
-static int const STOP_AFTER_KEYS_FOUND = 100;
+static int const STOP_AFTER_KEYS_FOUND = 1;
 
 // how many times a gpu thread generates a public key in one go
 __device__ const int ATTEMPTS_PER_EXECUTION = 100000;
